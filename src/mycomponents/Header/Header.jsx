2466d6d7af55d47b5271617804c25cbb9a071773
Ganesh-Patel-U <ganesh.oficial158@gmail.com>
2024-07-04 15:15:02 +0530

    first done by ubantu

--- src/mycomponents/Header/Header.jsx
+++ src/mycomponents/Header/Header.jsx
@@ -1,25 +1,47 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
-import './Header.css'
+export default function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
-export default function Header(){
     return (
         <header className='header'>
             <div className="nav-left">
                 <img src="https://flowbite.com/docs/images/logo.svg" alt="logo image" />
                 <span className="logotext">GeekFoods</span>
             </div>
-            <div className="nav-center">
+            <div className={`nav-center ${isOpen ? 'open' : ''}`}>
                 <ul className='nav-list'>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Quote</a></li>
-                    <li><a href="#">Resturents</a></li>
-                    <li><a href="#">Foods</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/quote">Quote</Link></li>
+                    <li><Link to="/resturents">Resturents</Link></li>
+                    <li><Link to="/foods">Foods</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </div>
             <div className="nav-right">
-                 <button id='startbtn'>Get Started</button>
+                <button id='startbtn'>Get Started</button>
             </div>
+            <div className="hamburger" onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            {isOpen && (
+                <div className="dropdown-menu">
+                    <ul>
+                        <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+                        <li><Link to="/quote" onClick={toggleMenu}>Quote</Link></li>
+                        <li><Link to="/resturents" onClick={toggleMenu}>Resturents</Link></li>
+                        <li><Link to="/foods" onClick={toggleMenu}>Foods</Link></li>
+                        <li><Link to="/contact" onClick={toggleMenu}>Contact</Link></li>
+                    </ul>
+                </div>
+            )}
         </header>
-    )
+    );
 }
